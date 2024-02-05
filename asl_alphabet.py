e112c28466f0a95a85eddbf23bb30183495a99f8
knguyen-hub <54388203+knguyen-hub@users.noreply.github.com>
2024-02-05 00:49:02 -0800

    Add files via upload

---
+++ asl_alphabet.py
@@ -0,0 +1,220 @@
+import cv2
+import numpy as np
+
+import onnx
+import onnxruntime as ort
+
+import random
+
+from time import time
+
+def nothing1(x):
+    pass
+
+def get_random_response():
+    rand_value = random.random()
+    if rand_value < 0.25:
+        return 'Good job!'
+    elif rand_value < 0.5:
+        return 'Correct!'
+    elif rand_value < 0.75:
+        return 'Keep it up!'
+    elif rand_value < 1.0:
+        return 'Right!'
+
+is_random = False
+def randomize_button_callback(state):
+    if state == 1:
+        is_random = True
+    else:
+        is_random = False
+
+cv2.namedWindow('Sign Language Translator')
+cv2.createTrackbar('X', 'Sign Language Translator', 50, 1180, nothing1)
+cv2.createTrackbar('Y', 'Sign Language Translator', 150, 620, nothing1)
+cv2.createTrackbar('Size', 'Sign Language Translator', 130, 300, nothing1)
+cv2.createTrackbar('Quiz', 'Sign Language Translator', 1, 1, nothing1)
+cv2.createTrackbar('Random', 'Sign Language Translator', 0, 1, nothing1)
+
+
+ort_session = ort.InferenceSession("signlanguage1.onnx")
+
+index_to_letter = list('ABCDEFGHIKLMNOPQRSTUVWXY')
+
+cap = cv2.VideoCapture(0)
+
+help_frame = np.zeros((620, 1180, 3), np.uint8)
+cv2.rectangle(help_frame, (0,0), (1180, 620), (240,240,240), -1)
+
+cv2.putText(help_frame, "Welcome to the ASL Alphabet Application!",
+            (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "This program uses a machine learning model to classify sign language letters.",
+            (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "Place your hand within the black square shown on the screen",
+            (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "Adjust this square by using the X, Y, and Size trackbars",
+            (20, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "For best results, use with a moderately clear background and lighting",
+            (20, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "The program displays which letter to sign in the upper right corner.",
+            (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "To randomize the order, toggle the Random slider.",
+            (20, 350), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "I apologize for any bugs, this program is still very much a work in progress :)",
+            (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+
+
+cv2.putText(help_frame, "Kaitlin Nguyen, Advanced Programming Topics (June 2021)",
+            (20, 560), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), thickness=2)
+
+cv2.putText(help_frame, "Press the spacebar to close this window",
+            (20, 600), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), thickness=2)
+
+run_program = True
+# check if camera is available
+while (not cap.isOpened()):
+    message_img = np.zeros((620,1180,3), np.uint8)
+
+    cv2.putText(message_img, "Camera not available.", (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                (255, 255, 255), thickness=2)
+    cv2.putText(message_img, "Make sure your camera is not already in use, then press the spacebar.",
+                (20, 180), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
+    cv2.putText(message_img, "The program will then begin shortly.", (20, 230),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
+    
+    cv2.imshow("Camera not available", message_img)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 32:
+        cap = cv2.VideoCapture(0)
+        if cap.isOpened():
+            cv2.destroyWindow("Camera not available")
+            break
+    elif k == 27:
+        run_program = False
+        break
+
+quiz_index = 0
+is_correct = False
+initial_time = time()
+quiz_initial_time = time()
+
+showing_help = True
+index_list = []
+
+while(run_program):
+
+    xCrop = cv2.getTrackbarPos('X', 'Sign Language Translator')
+    yCrop = cv2.getTrackbarPos('Y', 'Sign Language Translator')
+    zoom = cv2.getTrackbarPos('Size', 'Sign Language Translator')
+    quiz = cv2.getTrackbarPos('Quiz', 'Sign Language Translator')
+    is_random = cv2.getTrackbarPos('Random', 'Sign Language Translator')
+    
+    # get and crop frame
+    _, frame = cap.read()
+
+    
+    display = frame
+    x = frame[yCrop: yCrop+zoom, xCrop: xCrop+zoom]
+    
+    x = cv2.cvtColor(x, cv2.COLOR_RGB2GRAY)
+    x = cv2.resize(x, (28, 28))
+
+    # find mean and standard deviation of pixel values
+    mean, std = x.mean(), x.std()
+
+    # normalize pixel values
+    x = (x - mean) / std
+    np.clip(x, -1, 1, x)
+    x = (x + 1.0) / 2.0
+
+    x = x.reshape(1, 1, 28, 28).astype(np.float32)
+
+    # put through network
+    y = ort_session.run(None, {'input': x})[0]
+
+    # get letter
+    index = np.argmax(y, axis=1)
+    letter = index_to_letter[int(index)]
+
+    # quiz program
+    if quiz == 1:   
+        quiz_letter = index_to_letter[quiz_index]
+        if len(index_list) == 24:
+            index_list.clear()
+        index_list.append(quiz_index)
+        # check if letter is correct
+        if quiz_letter == letter and not is_correct:
+            quiz_initial_time = time()
+            is_correct = True
+            response = get_random_response()
+
+        if time() < quiz_initial_time + 1 and is_correct:
+            display = cv2.flip(display, 1)
+            cv2.putText(display, response, (100, 100), cv2.FONT_HERSHEY_SIMPLEX,
+                    2.0, (0, 255, 0), thickness=2)
+            display = cv2.flip(display, 1)
+
+        
+        elif time() > quiz_initial_time + 1 and is_correct:
+            if is_random == 1:
+
+                
+                unique_letter = False
+                while not unique_letter:
+                    if quiz_index in index_list:
+                        quiz_index  = int(random.random() * 24)
+                    else:
+                        unique_letter = True
+
+                
+            else :
+                quiz_index += 1
+
+            if quiz_index > 23:
+                quiz_index = 0
+            is_correct = False
+
+        
+        
+        display = cv2.flip(display, 1)
+        cv2.putText(display, quiz_letter, (500, 125), cv2.FONT_HERSHEY_SIMPLEX,
+                2.0, (0, 0, 0), thickness=2)
+
+    # detecting program
+    else:
+        display = cv2.flip(display, 1)
+        cv2.putText(display, letter, (100, 125), cv2.FONT_HERSHEY_SIMPLEX,
+                2.0, (0, 0, 0), thickness=2)
+
+    # display
+    display = cv2.flip(display, 1)
+    cv2.rectangle(display, (xCrop, yCrop), (xCrop + zoom, yCrop + zoom), (0,0,0), 3)
+    display = cv2.flip(display, 1)
+
+    if time() < initial_time + 3:
+        cv2.putText(display, "Press h for help, esc to quit", (10, 20), cv2.FONT_HERSHEY_PLAIN,
+                1.5, (255, 255, 255), thickness=2)
+        
+    
+    cv2.imshow("Sign Language Translator", display)
+
+    
+    k = cv2.waitKey(5) & 0xFF
+
+    if k == 104 or k == 72:
+        cv2.imshow("Help", help_frame)
+        showing_help = True
+    if k == 32 and showing_help:
+        cv2.destroyWindow("Help")
+        
+    if k == 27:
+        break
+cv2.destroyAllWindows()
+cap.release()
---
+++ project_attempt_1.py
@@ -0,0 +1,215 @@
+from torch.utils.data import Dataset
+from torch.autograd import Variable
+import csv
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+from typing import List
+
+class Net(nn.Module):
+    # constructor
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # convolution layers
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=6, kernel_size=3)
+        self.conv3 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3)
+
+        # max pool and ReLU layers
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu = nn.ReLU()
+
+        # fully connected layers
+        self.fully_connected_1 = nn.Linear(16 * 5 * 5, 120)
+        self.fully_connected_2 = nn.Linear(120, 48)
+        self.fully_connected_3 = nn.Linear(48, 24)
+
+
+    # forward propagation method
+    def forward(self, x):
+        # first convolution layer, and ReLU
+        x = self.conv1(x)
+        x = self.relu(x)
+
+        # second convolution, ReLU, max pool
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        # third convolution, ReLU, max pool
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = x.view(-1, 16 * 5 * 5)
+        
+        # linear fully connected layers
+        x = self.fully_connected_1(x)
+        x = self.relu(x)
+
+        x = self.fully_connected_2(x)
+        x = self.relu(x)
+
+        x = self.fully_connected_3(x)
+
+        return x
+
+class SignLanguageDataset(Dataset):
+
+    # reads samples and annotations from csv
+    def read_annotations_samples(path: str):
+        mapping = list(range(25))
+        mapping.pop(9)
+
+        #initialize arrays for both labels and samples
+        labels, samples = [], []
+        with open(path) as f:
+            _ = next(f)
+            for line in csv.reader(f):
+                # the first number in the line is the label
+                label = int(line[0])
+                labels.append(mapping.index(label))
+                # the rest of the numbers are the 784 pixel values
+                samples.append(list(map(int, line[1:])))
+        return labels, samples
+
+    def __init__(self,
+                 path: str="data/sign_mnist_train.csv",
+                 mean: List[float]=[0.485],
+                 std: List[float]=[0.229]):
+
+        # get samples and labels from above method
+        labels, samples = SignLanguageDataset.read_annotations_samples(path)
+        # convert to numpy arrays
+        self._samples = np.array(samples, dtype=np.uint8).reshape((-1, 28, 28, 1))
+        self._labels = np.array(labels, dtype=np.uint8).reshape((-1, 1))
+
+        self._mean = mean
+        self._std = std
+
+    def __len__(self):
+        return len(self._labels)
+
+    def __getitem__(self, index):
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomResizedCrop(28, scale=(0.8, 1.2)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=self._mean, std=self._std)])
+        return {
+            'image': transform(self._samples[index]).float(),
+            'label': torch.from_numpy(self._labels[index]).float()
+        }
+
+def get_train_loader(batch_size=32):
+    trainset = SignLanguageDataset('data/sign_mnist_train.csv')
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size,
+                                                shuffle=True)
+    testset = SignLanguageDataset('data/sign_mnist_test.csv')
+    testloader = torch.utils.data.DataLoader(testset, batch_size = batch_size,
+                                             shuffle=False)
+    return trainloader, testloader
+    
+
+def train(net, criterion, optimizer, trainloader, epoch):
+
+    net.train()
+    
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs = Variable(data['image'].float())
+        labels = Variable(data['label'].long())
+        optimizer.zero_grad()
+
+        pred = net(inputs)
+        loss = criterion(pred, labels[:, 0])
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 0:
+            print("[" + str(epoch + 1) + ", " + str(i) + "] loss: " + str(running_loss / (i + 1)))
+
+    # average loss
+    running_loss /= (i + 1)
+    return running_loss
+
+
+# -------------------VALIDATION---------------------- #
+def evaluate(outputs: Variable, labels: Variable) -> float:
+    y = labels.numpy()
+    y_pred = np.argmax(outputs, axis=1)
+    return float(np.sum(y_pred == y))
+
+def batch_evaluate(
+        net: Net,
+        dataloader: torch.utils.data.DataLoader) -> float:
+    score = 0.0
+    n = 0.0
+    for batch in dataloader:
+        n += len(batch['image'])
+        outputs = net(batch['image'])
+        if isinstance(outputs, torch.Tensor):
+            outputs = outputs.detach().numpy()
+        score += evaluate(outputs, batch['label'][:, 0])
+    return score / n
+
+# setting up parameters
+net = Net().float()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+trainloader, testloader = get_train_loader()
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+print("Data loaded")
+
+train_acc = list()
+train_loss = list()
+val_acc = list()
+best_val_loss = 1
+epoch = 3
+
+for i in range(epoch):
+    # train and evaluate
+    loss = train(net, criterion, optimizer, trainloader, i)
+    accuracy = batch_evaluate(net, trainloader)
+    val_accuracy = batch_evaluate(net, testloader)
+    # add loss and accuracy values to respective lists
+    train_loss.append(loss)
+    train_acc.append(accuracy)
+    val_acc.append(val_accuracy)
+    # change learning rate of net
+    scheduler.step()
+
+    # print 
+    print('-----TRAIN ACCURACY-----')
+    print(str(i+1) + ": " + str(train_acc))
+    print('-----TEST ACCURACY-----')
+    print(str(i+1) + ": " + str(val_acc))
+    print('------------------------')
+
+
+# save model parameters
+torch.save(net.state_dict(), "test_parameters.pth")
+
+# graphing
+fig=plt.figure(figsize=(20, 10))
+#plt.plot(np.arange(1, epoch+1), train_loss, label="Train loss")
+plt.plot(np.arange(1, epoch+1), train_acc, label="Train accuracy")
+plt.plot(np.arange(1, epoch+1), val_acc, label="Test accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.title("Accuracy Plots")
+plt.legend(loc='upper right')
+plt.show()
+
+train_loss = 0.0
+
+
+
+        
